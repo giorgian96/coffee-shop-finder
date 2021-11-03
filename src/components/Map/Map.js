65ee7144981863154ebad7671dd040b984ab10ac
giorgian96 <robescu.george96@gmail.com>
2021-11-03 15:10:25 +0200

    [5] Display shops and user location on map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,8 +1,19 @@
 import './Map.css';
+import { useEffect } from 'react';
 import { mapService } from '../../services/mapService';
 
 const Map = () => {
-  mapService.initMap();
+  useEffect(() => {
+    window.addEventListener('load', () => {
+      mapService.initMap();
+    });
+
+    return () => {
+      window.removeEventListener('load', () => {
+        mapService.initMap();
+      });
+    }
+  });
 
   return (
     <canvas id="map"></canvas>
--- src/services/mapService.js
+++ src/services/mapService.js
@@ -18,11 +18,13 @@ export const mapService = {
   },
 
   translateLongitudeToCanvasPx(longitude){
-    return (longitude + this.defaultWidth / 2) * this.mapSettings.scale;
+    let x = (longitude + this.mapSettings.defaultWidth / 2) * this.mapSettings.scale;
+    return x;
   },
 
   translateLatitudeToCanvasPx(latitude){
-    return (latitude + this.defaultHeight / 2) * this.mapSettings.scale;
+    let y = ((this.mapSettings.defaultHeight / 2) - latitude) * this.mapSettings.scale;
+    return y;
   },
 
   initMap(){
@@ -31,11 +33,31 @@ export const mapService = {
     ctx.canvas.width = this.getMapWidth();
     ctx.canvas.height = this.getMapHeight();
 
-    this.draw(ctx);
+    // draw some coffee shops
+    this.drawCoffeeShopAt(ctx, this.translateLongitudeToCanvasPx(25), this.translateLatitudeToCanvasPx(45));
+    this.drawCoffeeShopAt(ctx, this.translateLongitudeToCanvasPx(150), this.translateLatitudeToCanvasPx(30));
+    this.drawCoffeeShopAt(ctx, this.translateLongitudeToCanvasPx(-100), this.translateLatitudeToCanvasPx(50));
+    this.drawCoffeeShopAt(ctx, this.translateLongitudeToCanvasPx(-60), this.translateLatitudeToCanvasPx(-30));
+
+    // draw user's location
+    this.drawUserLocation(ctx, this.translateLongitudeToCanvasPx(25), this.translateLatitudeToCanvasPx(45));
   },
 
-  draw(ctx){
-    ctx.fillStyle = "green";
-    ctx.fillRect(10, 10, 150, 100);
+  drawCoffeeShopAt(ctx, xLon, yLat) {
+    ctx.beginPath();
+
+    ctx.moveTo(xLon - 10, yLat - 10);
+    ctx.lineTo(xLon + 10, yLat + 10);
+
+    ctx.moveTo(xLon + 10, yLat - 10);
+    ctx.lineTo(xLon - 10, yLat + 10);
+    ctx.stroke();
+  },
+
+  drawUserLocation(ctx, xLon, yLat){
+    ctx.fillStyle = "#ff0000";
+    ctx.beginPath();
+    ctx.arc(xLon, yLat, 10, 0, 2 * Math.PI);
+    ctx.fill();
   }
 }
